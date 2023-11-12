f290ed603b7d2ceb74125491aecf657bf68e3b5e
Gui113893 <gui.santos91@ua.pt>
2023-11-12 11:42:12 +0000

    test2

--- test.py
+++ test.py
@@ -6,12 +6,14 @@ def tes1():
 def tes2():
     # Path: test.py
     print("Test2")
+    return 2+2
 
 
 def main():
     n = tes1()
     print(n)
-    tes2()
+    t = tes2()
+    print(t)
 
 if __name__ == '__main__':
     main()
