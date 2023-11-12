cae286e2f48eabfac297889a59f05127bf5c6243
Gui113893 <gui.santos91@ua.pt>
2023-11-12 11:41:29 +0000

    fix

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
-def tes1():
+def test1():
     # Path: test.py
     print('tes1')
     return 1+1
 
-def tes2():
+def test2():
     # Path: test.py
     print("Test2")
 
 
 def main():
-    n = tes1()
+    n = test1()
     print(n)
-    tes2()
+    test2()
 
 if __name__ == '__main__':
     main()
