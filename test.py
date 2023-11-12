81434a5cbaae7cbbfcded5e43e3cb38d344f443d
Gui113893 <gui.santos91@ua.pt>
2023-11-12 11:35:49 +0000

    newThings

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 def tes1():
     # Path: test.py
     print('tes1')
+    return 1+1
 
 def tes2():
     # Path: test.py
@@ -8,7 +9,8 @@ def tes2():
 
 
 def main():
-    tes1()
+    n = tes1()
+    print(n)
     tes2()
 
 if __name__ == '__main__':
